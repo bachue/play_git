79d742d9a0d320e0fbbed07db57f9fe411877aec
Bachue Zhou <bachue.shu@gmail.com>
2015-10-16 18:21:46 +0800

    add read_object_file.rb

--- read_object_file.rb
+++ read_object_file.rb
@@ -1,4 +1,3 @@
-require_relative 'git_repo'
 require_relative 'git_object'
 require_relative 'git_commit'
 require_relative 'git_tree'
@@ -6,24 +5,16 @@ require_relative 'git_tag'
 
 def usage
   STDERR.puts <<-HELP
-#$0 sha1 [git_repo_path]
+#$0 index_file
   HELP
 end
 
-if ARGV[0].nil? || ARGV[0].empty?
+unless ARGV[0]
   usage
   exit!
 end
 
-sha1 = [ARGV[0]].pack 'H40'
-if sha1.bytesize != 20
-  usage
-  exit!
-end
-
-repo = ARGV[1].nil? || ARGV[1].empty? ? GitRepo.from_cwd : GitRepo.new(ARGV[1])
-object = repo.read_object sha1
-abort 'not found' unless object
+object = GitObject.read_from ARGV[0]
 
 puts <<-OUTPUT
 type: #{object.type}
